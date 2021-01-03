4d067db21277883bd03e6fd4152ec8768b1ec508
Deeksha D Bhandari <63086907+Deeksha-d-19@users.noreply.github.com>
2021-01-03 18:09:17 +0530

    Update dbconnect.php

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -2,7 +2,7 @@
 $db_host="localhost";
 $db_username="root";
 $db_password="";
-$db_name="quote_website";
+$db_name="contact";
 $db_connect=mysqli_connect($db_host,$db_username,$db_password,$db_name) or die();
 //check connection
 //if(mysqli_connect_error())
