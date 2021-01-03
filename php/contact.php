783b9a48769a941292f5ffa98234a82750c39202
Deeksha D Bhandari <63086907+Deeksha-d-19@users.noreply.github.com>
2021-01-03 18:08:52 +0530

    Update contact.php

--- php/contact.php
+++ php/contact.php
@@ -5,6 +5,8 @@
 <head>
   <title>Perceived Ideas</title>
     <link rel="stylesheet" type="text/css" href="contact.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
@@ -38,22 +40,18 @@
 </nav>
 </section>
 <!------contact form------->
-  <link rel="stylesheet" type="text/css" href="contact.css"><br>
-    <div class="contact-title">
-      <h1>Contact Us</h1>
-      <h2>We are happy to hear from you!</h2>
-    </div>
-<div class="contact-form">
-  <div class="container">
-  <form action="user_process.php" class="contact-form" method="post" name="user">
+<section class="contact-for"><br/>
+      <h1 class="deek">Contact Us!</h1>
+      <h2 class="deek">We are happy to hear from you!</h2>
+      <br/>
+    <form action="user_process.php" class="contact-form" method="post" name="user">
     <input name="name" type="text" class="form-control" placeholder="Name" required><br>
     <input name="subject" type="text" class="form-control" placeholder="Subject" required><br>
     <input name="email" type="email" class="form-control" placeholder="Email" required><br>
     <textarea name="message" class="form-control" placeholder="Message" row="5" required></textarea><br>
-    <input type="submit" class="form-control submit" value="Submit"><br>
+    <input type="submit" class="form-control-submit" value="Submit"><br/>
   </form>
-</div>
-</div>
+</section>
 <!--------footer------->
 <footer class="site-footer">
       <div class="container">
@@ -65,7 +63,7 @@
           </div>
           <div class="col-md-4 col-sm-6 col-xs-12">
             <div class="container">
-            <p align="right"><span style='font-size:20px''right-align:20px;'>&#9993; :</span> deekshadb99@gmail.com<br><span style='font-size:20px;'>&#9990; :</span>+91-7019631592 </p>
+            <p align="right"><span style='font-size:20px''align-right:20px;'>&#9993; :</span> deekshadb99@gmail.com<br><span style='font-size:20px;'>&#9990; :</span>+91-7019631592 </p>
           </div>
         </div>
 </footer>
